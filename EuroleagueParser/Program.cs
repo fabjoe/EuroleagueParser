610e8a1006dea1be4edcbd928f51c67116d5c50b
fabrizioc <fabrizioc@silversea.com>
2017-10-13 16:22:03 +0200

    Add project files.

---
+++ EuroleagueParser/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Linq;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace EuroleagueParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                WebClient webClient = new WebClient();
+                string page = webClient.DownloadString("http://www.euroleague.net/main/statistics?mode=Leaders&entity=Players&seasonmode=Single&seasoncode=E2017&cat=Valuation&agg=Accumulated");
+
+                HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+                doc.LoadHtml(page);
+                List<List<string>> table = doc.DocumentNode.SelectSingleNode("//table [not(descendant::table)]")
+                            .Descendants("tr")
+                            .Where(tr => tr.Elements("td").Count() > 1)
+                            .Select(tr => tr.Elements("td").Select(td => td.InnerText.Trim()).ToList())
+                            .ToList();
+
+                MapHtmlWithRanking(table);
+            }
+            catch (SystemException excep)
+            {
+
+            }
+
+        }
+
+        private static void MapHtmlWithRanking(List<List<string>> table)
+        {
+            foreach(List<string> info in table)
+            {
+                Ranking r = new Ranking(info);
+                SaveRanking(r);
+            }
+        }
+
+        private static void SaveRanking(Ranking r)
+        {
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["AppConnString"].ConnectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "usp_InsertRanking";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@PlayerId", r.PlayerId);
+                    cmd.Parameters.AddWithValue("@Ranking", r.RankingPosition);
+                    cmd.Parameters.AddWithValue("@PlayerName", r.PlayerName);
+                    cmd.Parameters.AddWithValue("@Team", r.Team);
+                    cmd.Parameters.AddWithValue("@NumberOfGames", r.NumberOfGames);
+                    cmd.Parameters.AddWithValue("@TotalRating", r.TotalRating);
+                    cmd.Parameters.AddWithValue("@AverageRating", r.AverageRating);
+                    cmd.Parameters.AddWithValue("@AverageRatingPer40Mins", r.AverageRatingPer40Mins);
+                    cmd.ExecuteNonQuery();
+
+                }
+            }
+        }
+    }
+}
---
+++ EuroleagueParser/Ranking.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EuroleagueParser
+{
+    public class Ranking
+    {
+        public Ranking(List<string> info)
+        {
+            PlayerId = info[1].Split(',')[1];
+            RankingPosition = Int32.Parse(info[0]);
+            PlayerName = info[1].Split(',')[1];
+            Team = info[2];
+            NumberOfGames = Int32.Parse(info[3]);
+            TotalRating = Decimal.Parse(info[4]);
+            AverageRating = Decimal.Parse(info[5]);
+            AverageRatingPer40Mins = Decimal.Parse(info[6]);
+        }
+        public string PlayerId { get; set; }
+        public int RankingPosition { get; set; }
+        public string PlayerName { get; set; }
+        public string Team { get; set; }
+        public int NumberOfGames { get; set; }
+        public decimal TotalRating { get; set; }
+        public decimal AverageRating { get; set; }
+        public decimal AverageRatingPer40Mins { get; set; }
+    }
+}
