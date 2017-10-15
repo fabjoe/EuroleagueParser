675feb634e4049a1705f80b270259dfbc9f2f5f9
Fabrizio Crespi <fabriziocrespi@gmail.com>
2017-10-15 22:08:31 +0200

    Looping all pages

--- EuroleagueParser/Program.cs
+++ EuroleagueParser/Program.cs
@@ -15,9 +15,26 @@ namespace EuroleagueParser
 
             try
             {
-                WebClient webClient = new WebClient();
-                string page = webClient.DownloadString("http://www.euroleague.net/main/statistics?mode=Leaders&entity=Players&seasonmode=Single&seasoncode=E2017&cat=Valuation&agg=Accumulated");
+                DeleteTables();
+                AcquireData();
+                
+            }
+            catch (SystemException excep)
+            {
 
+            }
+
+        }
+
+        private static void AcquireData()
+        {
+            WebClient webClient = new WebClient();
+            string webUrl = ConfigurationManager.AppSettings["WebSiteUrl"];
+            bool isFinished = false;
+            int newPage = 1;
+            while (!isFinished)
+            {
+                string page = webClient.DownloadString(webUrl);
                 HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
                 doc.LoadHtml(page);
                 List<List<string>> table = doc.DocumentNode.SelectSingleNode("//table [not(descendant::table)]")
@@ -25,14 +42,30 @@ namespace EuroleagueParser
                             .Where(tr => tr.Elements("td").Count() > 1)
                             .Select(tr => tr.Elements("td").Select(td => td.InnerText.Trim()).ToList())
                             .ToList();
-
+                
                 MapHtmlWithRanking(table);
+                if (table.Count < 50)
+                    isFinished = true;
+                newPage++;
+                string pageNumber = newPage.ToString().PadLeft(3, '0');
+                webUrl = ConfigurationManager.AppSettings["WebSiteUrl"] + "&page=" + pageNumber;
             }
-            catch (SystemException excep)
+
+        }
+
+        private static void DeleteTables()
+        {
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["AppConnString"].ConnectionString))
             {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "usp_DeleteRanking";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
 
+                }
             }
-
         }
 
         private static void MapHtmlWithRanking(List<List<string>> table)
